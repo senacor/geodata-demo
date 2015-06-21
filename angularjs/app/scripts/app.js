0ef31754fc1e4579a7220885ad5d687f2d300e45
Martin Blume <martin.blume@senacor.com>
2015-06-21 15:45:51 +0200

    Implemented navigation.

--- angularjs/app/scripts/app.js
+++ angularjs/app/scripts/app.js
@@ -9,4 +9,26 @@
  * Main module of the application.
  */
 angular
-    .module('angularjsApp', []);
+    .module('angularjsApp', ['ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider.
+            when('/view1', {
+                templateUrl: 'views/view1.html',
+                controller: 'View1Ctrl'
+            }).
+            when('/view2', {
+                templateUrl: 'views/view2.html',
+                controller: 'View2Ctrl'
+            }).
+            when('/view3', {
+                templateUrl: 'views/view3.html',
+                controller: 'View3Ctrl'
+            }).
+            when('/view4', {
+                templateUrl: 'views/view4.html',
+                controller: 'View4Ctrl'
+            }).
+            otherwise({
+                redirectTo: '/view1'
+            });
+    });
---
+++ angularjs/app/scripts/controllers/view2.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularjsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularjsApp
+ */
+angular.module('angularjsApp', ['ngRoute'])
+    .controller('View2Ctrl', function ($scope) {
+    });
