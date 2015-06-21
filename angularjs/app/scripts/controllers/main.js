4db88bbd905c89b73db253c8f4ae4c0b094e3114
Martin Blume <martin.blume@senacor.com>
2015-06-21 15:12:48 +0200

    - Added toggle button to header. - Removed not needed code.

--- angularjs/app/scripts/controllers/main.js
+++ angularjs/app/scripts/controllers/main.js
@@ -8,10 +8,5 @@
  * Controller of the angularjsApp
  */
 angular.module('angularjsApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope) {
+    });
