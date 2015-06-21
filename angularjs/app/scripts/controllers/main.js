4b4ead895297955c4f3a21c8e48475588b8f9726
Martin Blume <martin.blume@senacor.com>
2015-06-21 15:27:18 +0200

    Added and used angular-route dependency.

--- angularjs/app/scripts/controllers/main.js
+++ angularjs/app/scripts/controllers/main.js
@@ -7,6 +7,16 @@
  * # MainCtrl
  * Controller of the angularjsApp
  */
-angular.module('angularjsApp')
+angular.module('angularjsApp', ['ngRoute'])
     .controller('MainCtrl', function ($scope) {
+    })
+    .config(function ($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
     });
