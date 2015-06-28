387edac8e3cd9d37be71a57fec15553b61d2114b
Martin Blume <martin.blume@senacor.com>
2015-06-28 19:24:21 +0200

    Used factory to call postalCode service

--- angularjs/app/scripts/controllers/postalcodeLookup.js
+++ angularjs/app/scripts/controllers/postalcodeLookup.js
@@ -8,14 +8,20 @@
  * Controller of the angularjsApp
  */
 angular.module('angularjsApp')
-    .controller('PostalcodeCtrl', function ($scope, $http) {
+    .controller('PostalcodeCtrl', function ($scope, $http, lookupPostalcodesFactory) {
         $scope.search = function () {
-            $http.get('http://api.geonames.org/findNearbyPostalCodesJSON?country=DE&radius=10&username=demo&postalcode=' + $scope.postalCode)
-                .success(function (response) {
-                    if (response.status) {
-                        console.error(response.status.message);
-                    }
-                    $scope.codes = response.postalCodes;
-                });
-        }
+            $scope.codes = lookupPostalcodesFactory.lookup($scope.postalCode);
+        };
+    }).factory('lookupPostalcodesFactory', function ($http) {
+        return {
+            lookup: function (postalCode) {
+                $http.get('http://api.geonames.org/findNearbyPostalCodesJSON?country=DE&radius=10&username=demo&postalcode=' + postalCode)
+                    .success(function (response) {
+                        if (response.status) {
+                            console.error(response.status.message);
+                        }
+                        return response.postalCodes;
+                    });
+            }
+        };
     });
