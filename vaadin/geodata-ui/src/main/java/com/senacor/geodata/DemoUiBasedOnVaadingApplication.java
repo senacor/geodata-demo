b0f42103c6563528c8760c0add8b219cc0259464
David Schmitz <david.schmitz@senacor.com>
2015-08-05 11:07:49 +0200

    Added request logging w/o payload

--- vaadin/geodata-ui/src/main/java/com/senacor/geodata/DemoUiBasedOnVaadingApplication.java
+++ vaadin/geodata-ui/src/main/java/com/senacor/geodata/DemoUiBasedOnVaadingApplication.java
@@ -3,10 +3,25 @@ package com.senacor.geodata;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 @EnableHystrix
 public class DemoUiBasedOnVaadingApplication {
+
+    @org.springframework.context.annotation.Configuration
+    public static class Configuration {
+        @Bean
+        public CommonsRequestLoggingFilter requestLoggingFilter() {
+            CommonsRequestLoggingFilter crlf = new CommonsRequestLoggingFilter();
+            crlf.setIncludeClientInfo(true);
+            crlf.setIncludeQueryString(true);
+//            crlf.setIncludePayload(true);
+            return crlf;
+        }
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DemoUiBasedOnVaadingApplication.class, args);
     }
